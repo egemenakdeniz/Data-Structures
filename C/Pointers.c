405c950ecb3885d6d1f4e31928c457a261598932
Egemen <117409203+egemenakdeniz@users.noreply.github.com>
2025-03-02 15:25:52 +0300

    Update Pointers.c

--- C/Pointers.c
+++ C/Pointers.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    int *ptr = &x;  // Pointer storing the address of x
+
+    printf("Value of x: %d\n", x);
+    printf("Address of x: %p\n", (void*)&x);
+    printf("Pointer Address: %p\n", (void*)ptr);
+    printf("Pointer Dereference: %d\n", *ptr);
+
+    return 0;
+}
 
